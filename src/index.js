e0c0508541ad8a9346a58985e0dd385237e89e39
Javier Izquierdo <JIZQUIERDO@claro.com.ar>
2020-10-09 21:03:07 -0300

    Se cambia de lugar el router para ver si se corrige un fix

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ const store = storeFactory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
